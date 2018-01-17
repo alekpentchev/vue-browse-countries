425d780fa931b05064edcbb008075f293af2e784
Aleksander Pentchev <aleksander.pentchev@execon.pl>
2018-01-17 21:42:50 +0100

    Init commit
    Vue-cli used to generate the project boilerplate.
    Added bootstrap, simple input, two-way binding and props on a child

---
+++ vue-browse-countries/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-md-12">
+          <input type="text" v-model="msg">
+        </div>
+
+        <div class="col-md-6">
+          <SearchResults :search-results="msg"/>
+        </div>
+
+        <div class="col-md-6">
+          <span>Some example content</span>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SearchResults from './components/SearchResults';
+
+export default {
+  name: 'App',
+  components: {
+    SearchResults,
+  },
+  data() {
+    return {
+      msg: 'Search for a country',
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-browse-countries/src/components/SearchResults.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>{{ searchResults }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchResults',
+  props: ['searchResults'],
+  data() {
+    return {
+      msg: 'Look for a country or its\' capital',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
